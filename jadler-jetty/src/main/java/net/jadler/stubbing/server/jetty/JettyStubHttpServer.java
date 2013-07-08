50d0964e704590b26b7fe4ee07dfaabda7b7b04d
Jan Dudek <jan.dudek.jr@gmail.com>
2013-07-08 10:24:52 +0200

    Jetty server refactoring. Closes #5

--- jadler-jetty/src/main/java/net/jadler/stubbing/server/jetty/JettyStubHttpServer.java
+++ jadler-jetty/src/main/java/net/jadler/stubbing/server/jetty/JettyStubHttpServer.java
@@ -6,37 +6,37 @@ package net.jadler.stubbing.server.jetty;
 
 import net.jadler.RequestManager;
 import net.jadler.stubbing.server.StubHttpServer;
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.DefaultHandler;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.nio.SelectChannelConnector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.commons.lang.Validate;
 import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
 
 
 /**
- * Default stub http server implementation using <a href="http://jetty.codehaus.org/jetty/" target="_blank">Jetty</a>
- * as an http server.
+ * Default stub http server implementation using Jetty as an http server.
  */
 public class JettyStubHttpServer implements StubHttpServer {
 
     private static final Logger logger = LoggerFactory.getLogger(JettyStubHttpServer.class);
     private final Server server;
-    private final Connector selectChannelConnector;
+    private final Connector httpConnector;
 
     public JettyStubHttpServer() {
         this(0);
     }
+    
 
     public JettyStubHttpServer(final int port) {
         this.server = new Server();
-        this.selectChannelConnector = new SelectChannelConnector();
-        selectChannelConnector.setPort(port);
-        server.addConnector(selectChannelConnector);
+        this.server.setSendServerVersion(false);
+        
+        this.httpConnector = new SelectChannelConnector();
+        this.httpConnector.setPort(port);
+        server.addConnector(this.httpConnector);
     }
+    
 
     /**
      * {@inheritDoc}
@@ -45,10 +45,9 @@ public class JettyStubHttpServer implements StubHttpServer {
     public void registerRequestManager(final RequestManager ruleProvider) {
         Validate.notNull(ruleProvider, "ruleProvider cannot be null");
 
-        final HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[] {new StubHandler(ruleProvider), new DefaultHandler() });
-        server.setHandler(handlers);
+        server.setHandler(new JadlerHandler(ruleProvider));
     }
+    
 
     /**
      * {@inheritDoc}
@@ -70,12 +69,13 @@ public class JettyStubHttpServer implements StubHttpServer {
         server.stop();
         logger.debug("jetty stopped");
     }
+    
 
     /**
      * {@inheritDoc}
      */
     @Override
     public int getPort() {
-        return selectChannelConnector.getLocalPort();
+        return httpConnector.getLocalPort();
     }
 }
